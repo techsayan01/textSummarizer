65f3855677c13643fb2a06cd993b02b530186aff
Sayan Biswas <techsayan@techsayan.local>
2019-03-12 15:26:33 +0530

    added command line file entry

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import sumy
+import os
+import sys
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
@@ -6,13 +8,16 @@ from sumy.summarizers.luhn import LuhnSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
 
 #import the file
-file = "refer.txt"
-
+file = sys.argv[1]
+if os.stat(file).st_size == 0 : 
+	file = "refer.txt"
+	if os.stat(file).st_size == 0 : 
+		return
 #tokenize the file
 parser = PlaintextParser.from_file(file, Tokenizer("english"))
 
 # Check the number of characters in the string to understand the reduction
-text  = open("refer.txt", "r")
+text  = open(file, "r")
 count = len((text.read()).split())
 
 # Calculate lines
