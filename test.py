2b08a2eebf2492e30c5a5c1aa211f6db62e33dbc
Sayan Biswas <techsayan@techsayan.local>
2019-03-12 15:10:24 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,40 @@
+import sumy
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
+from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.lsa import LsaSummarizer
+
+#import the file
+file = "refer.txt"
+
+#tokenize the file
+parser = PlaintextParser.from_file(file, Tokenizer("english"))
+
+# Check the number of characters in the string to understand the reduction
+text  = open("refer.txt", "r")
+count = len((text.read()).split())
+
+# Calculate lines
+if count > 10000:
+	lines = 100
+else:
+	for i in range(10):
+		if i == 0:
+			continue
+		if count > i * 1000 and count < (i+1) * 1000:
+			lines = 20 + (i * 10)
+
+#LexRank model
+summarizer = LexRankSummarizer()
+
+#Luhn model
+# summarizer = LuhnSummarizer()
+
+#LSA model
+# summarizer = LsaSummarizer()
+
+summary = summarizer(parser.document,lines)
+
+for sentence in summary:
+	print(sentence)
